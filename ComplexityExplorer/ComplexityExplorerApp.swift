d920d865fffa7938fe26ce138382dace216269f1
Enrique <EGTech24@gmail.com>
2025-04-16 03:00:41 -0700

    Initial project commit

---
+++ ComplexityExplorer/ComplexityExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexityExplorerApp.swift
+//  ComplexityExplorer
+//
+//  Created by Enrique Gongora on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ComplexityExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
